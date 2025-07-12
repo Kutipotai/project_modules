478a8e2ecb7a536b20c87c85df7d30fbd3b8c710
Kutipotai <kutipotai@gmail.com>
2025-07-12 20:49:04 +0300

    add ProxyManager

--- project_modules/universal_func/requests_func.py
+++ project_modules/universal_func/requests_func.py
@@ -1,15 +1,18 @@
 import time
-
 import requests
 import urllib.parse
 import urllib.request
+import random
+import uuid
+import threading
+from fake_useragent import UserAgent
 
 # import ssl
 # ssl._create_default_https_context = ssl._create_stdlib_context
 # pip install -U 'requests[socks]'
 
 
-def _get_proxy(*, proxies):
+def _get_proxy(*, proxies, **kwargs):
     _proxies = dict()
     if proxies:
         _host = proxies.get('host')
@@ -285,5 +288,246 @@ def send_message_discord(*, msg, chat_id, token):
     return err, res
 
 
+def response_to_data(response, type_content, _dv=None):
+    err = None
+    try:
+        response.encoding = 'utf-8'
+        match type_content:
+            case 'text':
+                return err, response.text
+            case 'json':
+                return err, response.json()
+            case _:
+                return err, response
+    except:
+        err = f'response_to_data: data not {type_content} error!'
+    return err, _dv
+
+
+def get_check_connect(proxies=None, timeout=10):
+    try:
+        for url in ['https://www.google.com', 'https://api.ipify.org/?format=json', 'https://ipinfo.io/json']:
+            r = requests.get(url, proxies=proxies, timeout=timeout)
+            if r.status_code == 200:
+                return True
+        return False
+    except:
+        return False
+
+
+class FingerprintGenerator:
+    def __init__(self):
+        self.ua = UserAgent()
+
+        # Минимальные реальные значения
+        self.platforms = ["Win32", "Linux x86_64", "MacIntel"]
+        self.device_memory = [2, 4, 8, 16, 24, 32, 64]
+        self.hardware_threads = [2, 4, 8, 6, 12, 18, 24, 32]
+        self.timezones = [
+            "Europe/Moscow", "UTC", "America/New_York", "Asia/Tokyo", "Europe/Berlin", "America/Los_Angeles"
+        ]
+        self.viewports = ["1920x1080", "1440x900", "1366x768", "1536x864", "1280x720", "2560x1440"]
+        self.languages = {
+            "en-US": "en-US,en;q=0.9",
+            "ru-RU": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+            "fr-FR": "fr-FR,fr;q=0.9,en;q=0.8",
+        }
+        self.user_agent_keywords = {
+            "Win32": ["Windows NT", "Win64"],
+            "Linux x86_64": ["X11; Linux x86_64", "Linux; Android"],
+            "MacIntel": ["Macintosh"],
+        }
+
+    def _find_user_agent(self, platform):
+        keywords = self.user_agent_keywords.get(platform)
+        try:
+            for _ in range(100):  # max attempts
+                ua = self.ua.random
+                if any(k in ua for k in keywords):
+                    return ua
+        except Exception:
+            pass
+        # fallback
+        return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36"
+
+    def generate(self, platform=None, lang="en-US", timezone=None, viewport=None):
+        platform = platform if platform else random.choice(self.platforms)
+        user_agent = self._find_user_agent(platform=platform)
+        headers = {
+            "User-Agent": user_agent,
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": self.languages.get(lang, "en-US,en;q=0.9"),
+            "DNT": "1",
+            "Referer": "https://www.google.com",
+            "X-Request-ID": str(uuid.uuid4()),
+
+            # Расширенный fingerprint
+            "X-Platform": platform,
+            "X-Device-Memory": str(random.choice(self.device_memory)),
+            "X-HardwareConcurrency": str(random.choice(self.hardware_threads)),
+            "X-Viewport": viewport if viewport else random.choice(self.viewports),
+            "X-Timezone": timezone if timezone else random.choice(self.timezones),
+            "X-Requested-With": "XMLHttpRequest"
+        }
+        return headers
+
+
+class ProxyManager:
+    def __init__(
+            self, workers,
+            not_work_timeout=300,
+            no_internet_timeout=60,
+            request_timeout=5,
+            limit_errors=3,
+            platform=None,  # "Win32",
+            lang=None,  # "ru-RU",
+            timezone=None,  # "Europe/Moscow"
+            viewport=None,
+    ):
+
+        self.fingerprint_generator = FingerprintGenerator()
+        self.platform = platform
+        self.lang = lang
+        self.timezone = timezone
+        self.viewport = viewport
+
+        self.min_timeout = 0
+        self.internet_blocked_until = int(not get_check_connect()) * (int(time.time()) + no_internet_timeout)
+        self.not_work_timeout = not_work_timeout
+        self.no_internet_timeout = no_internet_timeout
+        self.request_timeout = request_timeout
+        self.limit_errors = limit_errors
+        self.proxies = dict()
+        for worker in workers:
+            proxies = _get_proxy(proxies=worker.get('proxies'))
+            headers = self.fingerprint_generator.generate(
+                platform=platform,
+                lang=lang,
+                timezone=timezone,
+                viewport=viewport,
+            )
+            session = requests.Session()
+            session.proxies.update(proxies)
+            session.headers.update(headers)
+            name = proxies.get('http')
+            if not name:
+                name = 'main'
+
+            self.proxies[name] = {
+                'session': session,
+                'headers': headers,
+                'proxies': proxies,
+                'timeout_until': 0,
+                'errors': 0,
+            }
+
+    def refresh_fingerprint(self, proxy):
+        headers = self.fingerprint_generator.generate(self.platform, self.lang, self.viewport, self.timezone)
+        session = self.proxies[proxy]['session']
+        session.headers.update(headers)
+        self.proxies[proxy]['headers'] = headers
+
+    def get_proxy(self):
+        uts = int(time.time())
+        available = [p for p, data in self.proxies.items() if data['timeout_until'] < uts]
+        if not available:
+            return None
+        return random.choice(available)
+
+    def get_min_timeout(self):
+        uts = int(time.time())
+        min_timeout = 0
+        min_timeout_list = [
+            data['timeout_until'] - uts for p, data in self.proxies.items()
+            if data['timeout_until'] >= uts
+        ]
+        if min_timeout_list:
+            min_timeout = min(min_timeout_list)
+        return max(min_timeout, self.internet_blocked_until)
+
+    def request(self, url, retries=3, type_content='raw', _dv=None, **kwargs):
+        err = None
+        code = 0
+        res = _dv
+        uts = int(time.time())
+
+        if self.internet_blocked_until > uts:
+            code = 1
+            err = f'ProxyManager.request: internet_blocked_until={self.internet_blocked_until - uts}'
+            return err, res, code
+        if self.internet_blocked_until > 0:
+            if not get_check_connect():
+                print("[!] Потеряно соединение с интернетом.")
+                self.internet_blocked_until = uts + self.no_internet_timeout
+                code = 1
+                err = f'ProxyManager.request: internet_blocked_until={self.internet_blocked_until - uts}'
+                return err, res, code
+            self.internet_blocked_until = 0
+
+        for attempt in range(1, retries + 1):
+            uts = int(time.time())
+            proxy = self.get_proxy()
+            if not proxy:
+                code = 1
+                err = f'ProxyManager.request: нет рабочих proxy 0/{len(self.proxies)}'
+                res = _dv
+                break
+            proxy_data = self.proxies[proxy]
+            session = proxy_data['session']
+            proxy_data['timeout_until'] = uts + self.request_timeout
+            try:
+                response = session.get(url, **kwargs)
+                if not (response.status_code in [200]):
+                    response = None
+            except Exception as e:
+                response = None
+
+            if response is None:
+                err = f'ProxyManager.request: Ошибка запроса через proxy || [{proxy}]'
+                code = 2
+                res = _dv
+                proxy_data['errors'] += 1
+                proxy_data['timeout_until'] = uts + self.request_timeout * 2
+            else:
+                if proxy_data['errors'] > 1:
+                    proxy_data['errors'] -= 1
+
+            if proxy_data['errors'] > self.limit_errors:
+                if not self.internet_blocked_until + uts < 0:
+                    if not get_check_connect():
+                        print("[!] Потеряно соединение с интернетом.")
+                        self.internet_blocked_until = uts + self.no_internet_timeout
+                        return err, res, code
+                    else:
+                        self.internet_blocked_until = -(uts + self.no_internet_timeout * 2)
+                if not get_check_connect(proxies=proxy_data['proxies']):
+                    print(f"[!] Прокси {proxy} не работает. Тайм-аут {self.not_work_timeout} сек.")
+                    proxy_data['timeout_until'] = uts + self.not_work_timeout
+                    self.refresh_fingerprint(proxy)
+                else:
+                    proxy_data['errors'] = 0
+
+            if response:
+                _err, res = response_to_data(response=response, type_content=type_content, _dv=_dv)
+                if _err:
+                    err = f'ProxyManager.{_err} || [{proxy}]'
+                    code = 3
+                    res = _dv
+                return err, res, code
+        if not self.internet_blocked_until + uts < 0:
+            if not get_check_connect():
+                print("[!] Потеряно соединение с интернетом.")
+                self.internet_blocked_until = uts + self.no_internet_timeout
+                code = 1
+                err = f'ProxyManager.request: internet_blocked_until={self.internet_blocked_until - uts}'
+                return err, res, code
+            else:
+                print("[!] Всё ок с интернетом.")
+                self.internet_blocked_until = -(uts + self.no_internet_timeout * 2)
+        return err, res, code
+
+
 if __name__ == '__main__':
+    # _fg = FingerprintGenerator()
+    print(FingerprintGenerator().generate())
     pass
