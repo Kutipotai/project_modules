93b453a8d12a867543b58af74786786c40521d6b
Kutipotai <kutipotai@gmail.com>
2025-09-01 19:52:04 +0300

    change db worker

--- project_modules/data_base_func/db_worker_conn.py
+++ project_modules/data_base_func/db_worker_conn.py
@@ -1,6 +1,16 @@
 import sqlite3
+from contextlib import contextmanager
 import psycopg2
 from psycopg2.extras import DictCursor, DictRow
+
+
+@contextmanager
+def _cursor(conn: sqlite3.Connection):
+    cursor = conn.cursor()
+    try:
+        yield cursor  # передаём курсор наружу
+    finally:
+        cursor.close()  # гарантированное закрытие
 
 
 def close_db(*, print_err=True, **kwargs):
@@ -19,19 +29,25 @@ def close_db(*, print_err=True, **kwargs):
     return
 
 
-def delete_db(*, cur=None, table_name=None, other_query=None, **kwargs):
+def delete_db(*, conn=None, table_name=None, other_query=None, **kwargs):
     err = None
+    is_conn = False if conn is None else True
     if not table_name:
         err = f'delete_db: {table_name} --> error arg:[table_name = str]'
         return err
-    if cur is None:
+    if conn is None:
         if settings_connect := kwargs.get('settings_connect'):
-            err, conn, cur = custom_init_bd(**settings_connect)
-            if err:
-                close_db(cur=cur, conn=conn, print_err=False)
+            db_name = settings_connect.get('db_name')
+            isolation_level = settings_connect.get('isolation_level')
+            if db_name is None:
+                err = f'delete_db: {db_name} --> {table_name} --> Неуказан "db_name"!'
                 return err
-            err = delete_db(cur=cur, table_name=table_name, other_query=other_query, **kwargs)
-            close_db(cur=cur, conn=conn, print_err=False)
+            try:
+                with sqlite3.connect(f'{db_name}', isolation_level=isolation_level) as conn:
+                    err = delete_db(conn=conn, table_name=table_name, other_query=other_query, **kwargs)
+            except Exception as e:
+                close_db(conn=conn)
+                err = f'delete_db: {db_name} --> {table_name}  --> Ошибка подключения к БД! {e}'
             return err
         else:
             err = f'delete_db: {table_name} --> error empty settings_connect'
@@ -42,35 +58,46 @@ def delete_db(*, cur=None, table_name=None, other_query=None, **kwargs):
         query = f'DELETE FROM {table_name} {other_query};'
         if kwargs.get('query'):
             query = kwargs.get('query')
-        cur.execute(query)
+        if kwargs.get('is_begin'):
+            conn.execute("BEGIN")  # вручную начинаем транзакцию
+        with _cursor(conn=conn) as cur:
+            cur.execute(query)
+        if is_conn:
+            conn.commit()
     except sqlite3.Error as e:
         return f'delete_db: {table_name} --> {e}'
     return err
 
 
-def write_db_many(*, cur=None, table_name=None, data=None, other_query=None, is_update=False, ignor_keys=None, **kwargs):
+def write_db_many(*, conn=None, table_name=None, data=None, other_query=None, is_update=False, ignor_keys=None, **kwargs):
     err = None
+    is_conn = False if conn is None else True
     if not data:
         return err
     if not table_name:
         err = f'write_db_many: {table_name} --> error arg:[table_name = str]'
         return err
-    if cur is None:
+    if conn is None:
         if settings_connect := kwargs.get('settings_connect'):
-            err, conn, cur = custom_init_bd(**settings_connect)
-            if err:
-                close_db(cur=cur, conn=conn, print_err=False)
+            db_name = settings_connect.get('db_name')
+            isolation_level = settings_connect.get('isolation_level')
+            if db_name is None:
+                err = f'write_db_many: {db_name} --> {table_name} --> Неуказан "db_name"!'
                 return err
-            err = write_db_many(
-                cur=cur,
-                table_name=table_name,
-                data=data,
-                other_query=other_query,
-                is_update=is_update,
-                ignor_keys=ignor_keys,
-                **kwargs
-            )
-            close_db(cur=cur, conn=conn, print_err=False)
+            try:
+                with sqlite3.connect(f'{db_name}', isolation_level=isolation_level) as conn:
+                    err = write_db_many(
+                        conn=conn,
+                        table_name=table_name,
+                        data=data,
+                        other_query=other_query,
+                        is_update=is_update,
+                        ignor_keys=ignor_keys,
+                        **kwargs
+                    )
+            except Exception as e:
+                close_db(conn=conn)
+                err = f'write_db_many: {db_name} --> {table_name}  --> Ошибка подключения к БД! {e}'
             return err
         else:
             err = f'write_db_many: {table_name} --> error empty settings_connect'
@@ -79,7 +106,6 @@ def write_db_many(*, cur=None, table_name=None, data=None, other_query=None, is_
         ignor_keys = list()
     if other_query is None:
         other_query = ''  # 'WHERE True'
-    other_query = other_query.strip()
     arg_list = ','.join([f'{x}' for x in data[0]])
     query_insert = ','.join([f':{x}' for x in data[0]])
     query_update = ','.join([f'{x}=:{x}' for x in data[0] if not (x in ignor_keys)])
@@ -91,79 +117,94 @@ def write_db_many(*, cur=None, table_name=None, data=None, other_query=None, is_
     try:
         if kwargs.get('query'):
             query = kwargs.get('query')
-        cur.executemany(query, data)
-    except Exception as e:  # except sqlite3.Error as e:
+        if kwargs.get('is_begin'):
+            conn.execute("BEGIN")  # вручную начинаем транзакцию
+        with _cursor(conn=conn) as cur:
+            cur.executemany(query, data)
+        if is_conn:
+            conn.commit()
+    except Exception as e:
         err = f'write_db_meny: {table_name} --> {e}'
     return err
 
 
-def read_db_many(*, cur=None, table_name=None, many_query=None, other_query=None, keys=None, is_dict=True, **kwargs):
+def read_db_many(*, conn=None, table_name=None, many_query=None, other_query=None, keys=None, is_dict=True, **kwargs):
     err = None
-    def_value = list()
-    if is_dict:
-        def_value = dict()
+    def_value = dict() if is_dict else list()
+    is_conn = False if conn is None else True
     if not table_name:
         err = f'read_db_many: {table_name} --> error arg:[table_name = str]'
         return err, def_value, list()
-    if cur is None:
+    if conn is None:
         if settings_connect := kwargs.get('settings_connect'):
-            err, conn, cur = custom_init_bd(**settings_connect)
-            if err:
-                close_db(cur=cur, conn=conn, print_err=False)
+            db_name = settings_connect.get('db_name')
+            isolation_level = settings_connect.get('isolation_level')
+            if db_name is None:
+                err = f'read_db_many: {db_name} --> {table_name} --> Неуказан "db_name"!'
                 return err, def_value, list()
-            err, res_df, res_keys = read_db_many(
-                cur=cur,
-                table_name=table_name,
-                many_query=many_query,
-                other_query=other_query,
-                keys=keys,
-                is_dict=is_dict,
-                **kwargs
-            )
-            close_db(cur=cur, conn=conn, print_err=False)
+            try:
+                with sqlite3.connect(f'{db_name}', isolation_level=isolation_level) as conn:
+                    err, res_df, res_keys = read_db_many(
+                        conn=conn,
+                        table_name=table_name,
+                        many_query=many_query,
+                        other_query=other_query,
+                        keys=keys,
+                        is_dict=is_dict,
+                        **kwargs
+                    )
+            except Exception as e:
+                close_db(conn=conn)
+                err = f'read_db_many: {db_name} --> {table_name}  --> Ошибка подключения к БД! {e}'
             return err, res_df, res_keys
         else:
             err = f'read_db_many: {table_name} --> error empty settings_connect'
             return err, def_value, list()
     if other_query is None:
         other_query = ''  # 'WHERE True'
-    other_query = other_query.strip()
     try:
-        keys_item = f"{', '.join(str(x) for x in keys)}" if keys else '*'
-        if many_query:
-            kq = many_query.get('k')
-            vq = many_query.get('v')
-            if not all([kq, vq]):
-                err = f'read_db_many: {table_name} --> error arg:[data = dict(k=str, v=list)]'
-                return err, def_value, list()
-            if other_query and 'where' in other_query.lower():
-                other_query = other_query.lower().replace('where', 'and')
-            other_query = f"where {kq} in ({','.join(['?' for _ in vq])}) {other_query}"
-            query = f'SELECT {keys_item} FROM {table_name} {other_query};'
-            output_obj = cur.execute(query, vq)
-        else:
-            if other_query and not ('where' in other_query.lower()):
-                other_query = f'where {other_query}'
-            query = f'SELECT {keys_item} FROM {table_name} {other_query};'
-            if kwargs.get('query'):
-                query = kwargs.get('query')
-            output_obj = cur.execute(query)
-        res_keys = [tup[0] for tup in output_obj.description]
-        res_df = cur.fetchall()
+        other_query = other_query.strip()
+        if kwargs.get('is_begin'):
+            conn.execute("BEGIN")  # вручную начинаем транзакцию
+        with _cursor(conn=conn) as cur:
+            keys_item = f"{', '.join(str(x) for x in keys)}" if keys else '*'
+            if many_query:
+                kq = many_query.get('k')
+                vq = many_query.get('v')
+                if not all([kq, vq]):
+                    err = f'read_db_many: {table_name} --> error arg:[data = dict(k=str, v=list)]'
+                    return err, def_value, list()
+                if other_query and 'where' in other_query.lower():
+                    other_query = other_query.lower().replace('where', 'and')
+                other_query = f"where {kq} in ({','.join(['?' for _ in vq])}) {other_query}"
+                query = f'SELECT {keys_item} FROM {table_name} {other_query};'
+                output_obj = cur.execute(query, vq)
+            else:
+                if other_query and not ('where' in other_query.lower()):
+                    other_query = f'where {other_query}'
+                query = f'SELECT {keys_item} FROM {table_name} {other_query};'
+                if kwargs.get('query'):
+                    query = kwargs.get('query')
+                output_obj = cur.execute(query)
+
+            res_keys = [tup[0] for tup in output_obj.description]
+            res_df = cur.fetchall()
+        if is_conn:
+            conn.commit()
         if is_dict:
             res_df = [{res_keys[i]: v for i, v in enumerate(row)} for row in res_df]
-    except Exception as e:  # except sqlite3.Error as e:
+
+    except sqlite3.Error as e:
         res_df = def_value
         res_keys = list()
         err = f'read_db_many: {table_name} --> {e}'
     return err, res_df, res_keys
 
 
-def keys_db(*, cur, table_name, **kwargs):
+def keys_db(*, conn, table_name, **kwargs):
     err, res = None, dict()
     try:
-        cur.execute(f"PRAGMA table_info({table_name})")
-        res = {v[1]: i for i, v in enumerate(cur.fetchall())}
+        res = {v[1]: i for i, v in enumerate(conn.execute(f"PRAGMA table_info({table_name})").fetchall())}
     except Exception as e:
         res = dict()
         err = f'keys_db: {table_name} --> {e}'
@@ -184,27 +225,39 @@ def custom_default_option_db(*, cur, table_name, scheme, wal=False, **kwargs):
     return err
 
 
-def custom_init_bd(*, db_name=None, table_name=None, scheme=None, check_schemas=False, isolation_level=None, **kwargs):
-    err, conn, cur = None, None, None
+def init_bd(*, db_name=None, isolation_level=None, **kwargs):
+    err, conn = None, None
     if db_name is None:
-        err = f'custom_init_bd: {db_name} --> {table_name} --> Неуказан "db_name"!'
-        return err, conn, cur
-    if check_schemas and (not scheme or not table_name):
-        err = f'custom_init_bd: {db_name} --> {table_name} --> Неуказанна "scheme"!'
-        return err, conn, cur
+        err = f'init_bd: {db_name} --> Неуказан "db_name"!'
+        return err, conn
     try:
         conn = sqlite3.connect(f'{db_name}', isolation_level=isolation_level)
-        cur = conn.cursor()
-        if check_schemas:
+    except Exception as e:
+        close_db(conn=conn, print_err=False)
+        err = f'init_bd: {db_name} --> Ошибка подключения к БД! {e}'
+    return err, conn
+
+
+def create_bd(*, db_name=None, table_name=None, scheme=None, isolation_level=None, **kwargs):
+    if not scheme or not table_name:
+        err = f'create_bd: {db_name} --> {table_name} --> Неуказанна "scheme"!'
+        return err, None
+    err, conn = init_bd(db_name=db_name, isolation_level=isolation_level)
+    if err:
+        err = f'create_bd: --> {err}'
+        return err, conn
+    try:
+        with _cursor(conn=conn) as cur:
             if not custom_default_option_db(cur=cur, table_name=table_name, scheme=scheme, wal=True):
                 conn.commit()
             else:
-                close_db(cur=cur, conn=conn)
-                err = f'custom_init_bd: {db_name} --> {table_name} --> Ошибка подключения к БД!'
+                err = f'create_bd: {db_name} --> {table_name} --> Ошибка создания/подключения к БД!'
+        if err:
+            close_db(conn=conn)
     except Exception as e:
-        close_db(cur=cur, conn=conn)
-        err = f'custom_init_bd: {db_name} --> {table_name} --> Ошибка подключения к БД! {e}'
-    return err, conn, cur
+        err = f'create_bd: {db_name} --> {table_name} --> Ошибка подключения к БД! {e}'
+        close_db(conn=conn)
+    return err, conn
 
 
 def first_start_db(*, setting_database, print_err=True, **kwargs):
@@ -212,8 +265,8 @@ def first_start_db(*, setting_database, print_err=True, **kwargs):
     db_name = setting_database.get('db_name')
     tables = setting_database.get('tables', list())
     for table in tables:
-        err, conn, cur = custom_init_bd(db_name=db_name, check_schemas=True, **table)
-        close_db(cur=cur, conn=conn, print_err=False)
+        err, conn = create_bd(db_name=db_name, **table)
+        close_db(conn=conn, print_err=False)
         if err and print_err:
             print(f'first_start_db: {db_name} --> ', err)
     return err
