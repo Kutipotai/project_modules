a69a9cd624b703b6e493d4fd00cf396b027a058f
Kutipotai <kutipotai@gmail.com>
2025-12-01 16:57:50 +0300

    change db

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="project-modules",  # это имя будет использоваться в импортах
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(include=["project_modules", "project_modules.*"]),
     install_requires=[
         "requests",
