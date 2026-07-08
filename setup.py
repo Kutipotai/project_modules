6cc3daaa7eada1629c967638647a911e779c1912
Kutipotai <kutipotai@gmail.com>
2026-07-08 18:36:28 +0300

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="project-modules",  # это имя будет использоваться в импортах
-    version="0.1.16",
+    version="0.1.17",
     packages=find_packages(include=["project_modules", "project_modules.*"]),
     install_requires=[
         "requests",
